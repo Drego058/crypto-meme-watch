ac264de2e6bcdf786e24b347177f83048d8966d3
Drego058 <38866574+Drego058@users.noreply.github.com>
2025-04-14 14:53:32 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,17 @@
-fetch("http://localhost:8000/analyze")
+const backendUrl = window.location.hostname.includes("onrender.com")
+  ? "https://" + window.location.hostname + "/analyze"
+  : "http://localhost:8000/analyze";
+
+fetch(backendUrl)
   .then(res => res.json())
   .then(data => {
     const div = document.getElementById("results");
     data.results.forEach(item => {
       div.innerHTML += `<p><b>Text:</b> ${item.text}<br><b>Sentiment:</b> ${item.sentiment}<br><b>Prediction:</b> ${item.prediction}</p>`;
     });
+  })
+  .catch(err => {
+    console.error("Fout bij ophalen data:", err);
+    document.getElementById("results").innerHTML = "<p>Kan geen data laden van de backend.</p>";
   });
+
